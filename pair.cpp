cb072da8ba926bc00ec268889f7574e2cb7d8712
OnkarKhairnar23 <114229902+OnkarKhairnar23@users.noreply.github.com>
2022-10-28 23:06:16 +0530

    Add files via upload

---
+++ pair.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll int long long
+#define rep(a,b,i) for(ll i=a; i<b ; i++)
+#define co(a) cout<<a<<endl
+#define ci(a) cin>>a
+using namespace std;
+
+int main()
+{
+    //simple pair-
+    // pair<ll,ll> p={1,2};
+    // cout<<p.first<<" "<<p.second;
+
+    //nested pair-
+    // pair< ll, pair<ll,ll> > p={1,{2,3}};
+    // cout<<p.first<<" "<<p.second.first<<" "<<p.second.second;
+
+    //array of pair-
+    // pair<ll,ll> p[3]={{1,2},{3,4},{5,6}};
+    // rep(0,3,i){
+    //     cout<<p[i].first<<" "<<p[i].second<<endl;
+    // }
+
+    //vector of pair-
+    // vector<pair<ll,ll>> v={{1,2}};
+    // cout<<v[0].first<<" "<<v[0].second<<endl;
+    // v.push_back({3,4});
+    // cout<<v[1].first<<" "<<v[1].second<<endl;
+}           
